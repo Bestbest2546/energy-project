e103d9d813430ca92145205ab2a098cf08715136
Bestbest2546 <bst12546@gmail.com>
2024-01-05 17:49:42 +0700

    update

--- src/app/components/Energyflow/cardEnergyflow.js
+++ src/app/components/Energyflow/cardEnergyflow.js
@@ -8,7 +8,7 @@ export default function cardFlow() {
       {/* <div className="circlesolar"></div> */}
       <div className=" h-full flex justify-center items-start">
         <div
-          className={`flex justify-center flex-col items-center w-[${60}px] h-[${60}px] border-[1px] rounded-full border-yellow-500 outline outline-offset-4 outline-1 outline-yellow-200 bg-white`}
+          className={`flex justify-center flex-col items-center w-[60px] h-[60px] border-[1px] rounded-full border-yellow-500 outline outline-offset-4 outline-1 outline-yellow-200 bg-white`}
         >
           <Image src="/png/solar-panel.png" alt="" height={0} width={20} />
           <p className="text-[8px] font-bold font-serif text-gray-500">value</p>
@@ -17,7 +17,7 @@ export default function cardFlow() {
       </div>
       <div className=" h-full flex justify-between items-center">
         <div
-          className={`flex justify-center flex-col items-center w-[${60}px] h-[${60}px] border-[1px] rounded-full border-[#87DBDC] outline outline-offset-4 outline-1 outline-[#87dbdc7f] bg-white`}
+          className={`flex justify-center flex-col items-center w-[60px] h-[60px] border-[1px] rounded-full border-[#87DBDC] outline outline-offset-4 outline-1 outline-[#87dbdc7f] bg-white`}
         >
           <div className="flex justify-center items-center">
             <Image src="/png/empty-battery.png" alt="" height={0} width={20} />
@@ -27,7 +27,7 @@ export default function cardFlow() {
           <p className="text-[8px] font-bold font-serif text-gray-500">Watt</p>
         </div>
         <div
-          className={`flex justify-center flex-col items-center w-[${60}px] h-[${60}px] border-[1px] rounded-full border-purple-500 outline outline-offset-4 outline-1 outline-purple-200 bg-white`}
+          className={`flex justify-center flex-col items-center w-[60px] h-[60px] border-[1px] rounded-full border-purple-500 outline outline-offset-4 outline-1 outline-purple-200 bg-white`}
         >
           <Image src="/png/electric-tower.png" alt="" height={0} width={20} />
           <p className="text-[8px] font-bold font-serif text-gray-500">value</p>
@@ -36,7 +36,7 @@ export default function cardFlow() {
       </div>
       <div className=" h-full flex justify-center items-end">
         <div
-          className={`flex justify-center flex-col items-center w-[${60}px] h-[${60}px] border-[1px] rounded-full border-sky-500 outline outline-offset-4 outline-1 outline-sky-200 bg-white`}
+          className={`flex justify-center flex-col items-center w-[60px] h-[60px] border-[1px] rounded-full border-sky-500 outline outline-offset-4 outline-1 outline-sky-200 bg-white`}
         >
           <Image src="/png/energy-costs.png" alt="" height={0} width={20} />
           <p className="text-[8px] font-bold font-serif text-gray-500">value</p>
