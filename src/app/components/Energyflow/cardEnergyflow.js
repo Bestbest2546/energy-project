b7914772da1479fdfdb99fb723ce0295c61c4c20
Bestbest2546 <bst12546@gmail.com>
2024-01-05 17:45:36 +0700

    update

--- src/app/components/Energyflow/cardEnergyflow.js
+++ src/app/components/Energyflow/cardEnergyflow.js
@@ -1,16 +1,47 @@
+import Image from "next/image";
+// import './cardflow.css'
+
 export default function cardFlow() {
-    const circlesize = 60;
+  // const circlesize = 60;
   return (
-    <div className="w-[300px] h-[300px] flex flex-col p-2">
+    <div className="w-[300px] h-[300px] flex flex-col p-2 bg-[url('/energyflow.svg')] bg-no-repeat bg-center">
+      {/* <div className="circlesolar"></div> */}
       <div className=" h-full flex justify-center items-start">
-        <div className={`w-[${circlesize}px] h-[${circlesize}px] border-[1px] rounded-full border-yellow-500 outline outline-offset-4 outline-1 outline-yellow-200`}></div>
+        <div
+          className={`flex justify-center flex-col items-center w-[${60}px] h-[${60}px] border-[1px] rounded-full border-yellow-500 outline outline-offset-4 outline-1 outline-yellow-200 bg-white`}
+        >
+          <Image src="/png/solar-panel.png" alt="" height={0} width={20} />
+          <p className="text-[8px] font-bold font-serif text-gray-500">value</p>
+          <p className="text-[8px] font-bold font-serif text-gray-500">Watt</p>
+        </div>
       </div>
       <div className=" h-full flex justify-between items-center">
-        <div className={`w-[${circlesize}px] h-[${circlesize}px] border-[1px] rounded-full border-[#87DBDC] outline outline-offset-4 outline-1 outline-[#87dbdc7f]`}></div>
-        <div className={`w-[${circlesize}px] h-[${circlesize}px] border-[1px] rounded-full border-purple-500 outline outline-offset-4 outline-1 outline-purple-200`}></div>
+        <div
+          className={`flex justify-center flex-col items-center w-[${60}px] h-[${60}px] border-[1px] rounded-full border-[#87DBDC] outline outline-offset-4 outline-1 outline-[#87dbdc7f] bg-white`}
+        >
+          <div className="flex justify-center items-center">
+            <Image src="/png/empty-battery.png" alt="" height={0} width={20} />
+            <p className="absolute text-[7px] font-bold font-serif text-black">55%</p>
+          </div>
+          <p className="text-[8px] font-bold font-serif text-gray-500">value</p>
+          <p className="text-[8px] font-bold font-serif text-gray-500">Watt</p>
+        </div>
+        <div
+          className={`flex justify-center flex-col items-center w-[${60}px] h-[${60}px] border-[1px] rounded-full border-purple-500 outline outline-offset-4 outline-1 outline-purple-200 bg-white`}
+        >
+          <Image src="/png/electric-tower.png" alt="" height={0} width={20} />
+          <p className="text-[8px] font-bold font-serif text-gray-500">value</p>
+          <p className="text-[8px] font-bold font-serif text-gray-500">Watt</p>
+        </div>
       </div>
       <div className=" h-full flex justify-center items-end">
-        <div className={`w-[${circlesize}px] h-[${circlesize}px] border-[1px] rounded-full border-sky-500 outline outline-offset-4 outline-1 outline-sky-200`}></div>
+        <div
+          className={`flex justify-center flex-col items-center w-[${60}px] h-[${60}px] border-[1px] rounded-full border-sky-500 outline outline-offset-4 outline-1 outline-sky-200 bg-white`}
+        >
+          <Image src="/png/energy-costs.png" alt="" height={0} width={20} />
+          <p className="text-[8px] font-bold font-serif text-gray-500">value</p>
+          <p className="text-[8px] font-bold font-serif text-gray-500">Watt</p>
+        </div>
       </div>
     </div>
   );
--- src/app/components/Sidebar/sidebar.js
+++ src/app/components/Sidebar/sidebar.js
@@ -4,15 +4,15 @@ import { useSidebar } from "./useSidebar";
 const Sidebar = () => {
   const { isSidebarOpen, toggleSidebar } = useSidebar();
   return (
-    <div className="flex flex-row items-center lg:relative absolute" aria-label="Sidebar">
+    <div className="flex flex-row items-center lg:relative absolute h-full" aria-label="Sidebar">
       <div
-        className={`transition-all duration-500 ease-in-out border-2 h-screen ${
+        className={`transition-all duration-500 ease-in-out border-2 h-full ${
           isSidebarOpen ? "w-60 bg-white z-30" : "w-0"
         }`}
       ></div>
       <button
         onClick={toggleSidebar}
-        className="w-4 h-10 border-2 hover:bg-blue-500 rounded-r-lg "
+        className="w-4 h-10 border-2 bg-white hover:bg-blue-500 rounded-r-lg "
       >
         {isSidebarOpen ? "<" : ">"}
       </button>
